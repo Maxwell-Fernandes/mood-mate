2e283886e545a68932349b6b3c966f847c5c8115
Maxwell-Fernandes <fernandesmax082@gmail.com>
2025-08-16 09:49:39 +0530

    initial code for moodmate

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Button } from '@mui/material'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <Button variant='outlined'>
+        Hello World
+      </Button>
+    </div>
+    </>
+  )
+}
+
+export default App
